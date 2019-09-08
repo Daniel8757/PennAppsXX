f271f27176d189bbba7db7130af8d9a170849a32
George Diwan <36776618+gma8@users.noreply.github.com>
2019-09-08 08:12:55 -0400

    Add files via upload

---
+++ Extension_v2/content.js
@@ -0,0 +1,55 @@
+var picArray = [];
+var number = 0;
+var counter = 0;
+
+function repeatCheck(){
+	window.counter = window.counter+1;
+	var mycanvas = document.createElement("CANVAS");
+	var vids = document.getElementsByTagName('video');
+
+	for(var i = 0;i<vids.length;i++){
+  		vids.item(i).addEventListener('loadeddata', onLoaded);
+			
+		var video = vids.item(i);
+		var thecanvas = mycanvas;
+		draw(video, thecanvas);
+		
+	}
+
+	function draw(video, thecanvas){
+		var context = thecanvas.getContext('2d');
+		context.drawImage(video, 0, 0, thecanvas.width, thecanvas.height);
+		var ctx = thecanvas.getContext('2d');
+		
+		var img = new Image();
+		img.src = thecanvas.toDataURL();
+
+		window.picArray.push(img.src);
+
+		var myJSON = JSON.stringify(img.src);
+
+		let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(myJSON);
+
+    		let exportFileDefaultName = window.number.toString();
+		window.number = window.number+1;
+
+   		let linkElement = document.createElement('a');
+   		linkElement.setAttribute('href', dataUri);
+    		linkElement.setAttribute('download', exportFileDefaultName);
+   		linkElement.click();
+		window.picArray = [];
+	}
+	
+
+	function onLoaded(event){
+		console.log(event.target.src);
+	}
+
+	
+	if(window.counter<=10){
+		setTimeout(repeatCheck, 1000);
+	}
+
+}
+
+repeatCheck();
